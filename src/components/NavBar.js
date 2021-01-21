0eb040ef30a50d7fdab9bebf1dffb9f9655ae54e
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-20 21:32:38 -0500

    added router and files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from "react-router-dom";
 import * as ReactBootStrap from "react-bootstrap"
 
 const NavBar = () => {
     return (
+      <Router>
         <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <ReactBootStrap.Navbar.Brand href="#home">Read By Humans</ReactBootStrap.Navbar.Brand>
         <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
           <ReactBootStrap.Nav className="mr-auto">
-            <ReactBootStrap.Nav.Link href="#features">Record book</ReactBootStrap.Nav.Link>
+            <ReactBootStrap.Nav.Link href="#features">All Audibles</ReactBootStrap.Nav.Link>
             <ReactBootStrap.NavDropdown title="More" id="collasible-nav-dropdown">
               <ReactBootStrap.NavDropdown.Item href="#action/3.1">About</ReactBootStrap.NavDropdown.Item>
               <ReactBootStrap.NavDropdown.Item href="#action/3.2">Review</ReactBootStrap.NavDropdown.Item>
@@ -17,6 +23,7 @@ const NavBar = () => {
           </ReactBootStrap.Nav>
         </ReactBootStrap.Navbar.Collapse>
       </ReactBootStrap.Navbar>
+    </Router>
     )
 }
 export default NavBar
