dbdab32beb1b5157e1c3932b26f74d5336b25f94
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-21 16:06:36 -0500

    fetching data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home page</h1>
-        </div>
-    )
-}
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-export default Home
+  const [items, setItems] = useState([])
+
+  const fetchItems = async () => {
+    const data = await fetch("http://localhost:3001/audibles");
+
+    const items = await data.json()
+    console.log(items);
+  };
+
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import * as ReactBootStrap from "react-bootstrap"
-import{Link} from "react-router-dom"
+// import{Link} from "react-router-dom"
 
 const NavBar = () => {
     return (
-  
         <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <ReactBootStrap.Navbar.Brand href="/">Read By Humans</ReactBootStrap.Navbar.Brand>
         <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
