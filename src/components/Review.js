88722155671062af557af21342c39d4f02a84122
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-22 13:52:41 -0500

    created review page

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 const Review = () => {
-    return (
-        <div>
-            <h1>review page</h1>
-        </div>
-    )
-}
-export default Review
+
+    const [review, setReview] = useState([])
+    
+    useEffect(() => {
+        axios
+          .get("http://localhost:3001/reviews")
+          .then((resp) => setReview(resp.data));
+      }, []);
+
+  return (
+    <Jumbotron>
+      <Container>
+        <h1>review page</h1>
+        <p>{review.map((review) => (
+            <ul key={review.id}>
+                <li>Title: {review.title}</li>
+                <li>Description: {review.description}</li>
+                <li>Rating: {review.rating}</li>
+
+            </ul>
+
+        ))}</p>
+      </Container>
+    </Jumbotron>
+  );
+};
+export default Review;
