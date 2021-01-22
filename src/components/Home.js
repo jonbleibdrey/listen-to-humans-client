d59ccdfc1c86eb4dbb034f2c8907172b60212ee9
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-22 13:40:04 -0500

    added jumbotrona nd all the information for audibles

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
 
 const Home = () => {
   const [audible, setAudible] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:3001/audibles')
-    .then(resp => setAudible(resp.data))
-  }, [])
+    axios
+      .get("http://localhost:3001/audibles")
+      .then((resp) => setAudible(resp.data));
+  }, []);
 
   return (
-    <div>
+    <Jumbotron>
+        <Container>
       <h1>Home page</h1>
-      <div>{audible.map((audible) => (
-          <div key={audible.id}> {audible.title}</div>
-      ))} </div>
-    </div>
+      <div>
+        {audible.map((audible) => (
+          <ul key={audible.id}>
+            <li>Title: {audible.title}</li>
+            <li>By: {audible.by}</li>
+            <li>Language: {audible.language}</li>
+            <li>Audio: {audible.audio}</li>
+          </ul>
+        ))}
+      </div>
+      </Container>
+    </Jumbotron>
   );
 };
 
