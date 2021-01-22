ba7c7e246b92e082d6134701f1eeea9afc0e46c4
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-22 13:45:20 -0500

    created about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
 
 const About = () => {
-    return (
-        <div>
-            <h1> About page</h1>
-        </div>
-    )
-}
+  return (
+    <Jumbotron>
+      <Container>
+        <h1> About page</h1>
+        <p>
+          {" "}
+          This is a webpage for people to create librarys of read books. Where
+          it differs from audible is that you the user could take a book and
+          read it. You could create/read any book. then share it with other
+          people. where i see this shining is college students. It can be
+          challenging sometimes to find these books in audible form.{" "}
+        </p>
+      </Container>
+    </Jumbotron>
+  );
+};
 
-export default About
+export default About;
