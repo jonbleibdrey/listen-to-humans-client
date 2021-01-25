d468cd7f483f58e455a36763ccf6706adbb09959
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-25 16:30:49 -0500

    try to figure out post

--- src/components/AddAudible.js
+++ src/components/AddAudible.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Form, FormGroup, FormLabel, FormControl, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const AddAudible = () => {
+
   return (
     <Form>
       <FormGroup>
         <FormLabel> Title </FormLabel>
-        <FormControl type="text" placeholder="Enter Title"></FormControl>
+        <FormControl type="text" placeholder="Enter Title" ></FormControl>
         <FormLabel> By: </FormLabel>
-        <FormControl type="text" placeholder="Created By"></FormControl>
+        <FormControl type="text" placeholder="Created By" ></FormControl>
         <FormLabel> Language: </FormLabel>
-        <FormControl type="text" placeholder="Language read in"></FormControl>
+        <FormControl type="text" placeholder="Language read in" ></FormControl>
         <FormLabel> Audible: </FormLabel>
-        <FormControl type="text" placeholder="Audio file here"></FormControl>
+        <FormControl type="text" placeholder="Audio file here" ></FormControl>
       </FormGroup>
       <Button type="submit"> Submit </Button>
       <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 
+
 const Home = () => {
   const [audible, setAudible] = useState([]);
 
@@ -15,27 +16,35 @@ const Home = () => {
   }, []);
 
   return (
-    <Jumbotron>
-      <Container>
-        <h1>Home page</h1>
-        <div>
-          {audible.map((audible) => (
-            <ul key={audible.id}>
-              <li>Title: {audible.title}</li>
-              <li>By: {audible.by}</li>
-              <li>Language: {audible.language}</li>
-              <li>Audio: {audible.audio_file}</li>
-                <Link className='btn btn-warning mr-1' to={`/edit/${audible.id}`}> edit</Link>
+    <>
+      <Jumbotron>
+        <Container>
+          <h1>Home page</h1>
+          <div>
+            {audible.map((audible) => (
+              <ul key={audible.id}>
+                <li>Title: {audible.title}</li>
+                <li>By: {audible.by}</li>
+                <li>Language: {audible.language}</li>
+                <li>Audio: {audible.audio_file}</li>
+                <Link
+                  className="btn btn-warning mr-1"
+                  to={`/edit/${audible.id}`}
+                >
+                  {" "}
+                  edit
+                </Link>
                 <Button color="danger"> Delete</Button>
-            </ul>
-          ))}
-        </div>
-      </Container>
-      <Link to="/add" className="btn btn-primary ml-2">
-        {" "}
-        add book{" "}
-      </Link>
-    </Jumbotron>
+              </ul>
+            ))}
+          </div>
+        </Container>
+        <Link to="/add" className="btn btn-primary ml-2">
+          {" "}
+          add book{" "}
+        </Link>
+      </Jumbotron>
+    </>
   );
 };
 
