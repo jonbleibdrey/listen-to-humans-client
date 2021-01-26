2094e6917ea8b753316633ee943946feb918d9a1
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-01-26 14:24:33 -0500

    trying to post

--- src/components/AddAudible.js
+++ src/components/AddAudible.js
@@ -1,25 +1,71 @@
-import React, {useState, useEffect} from "react";
-import { Form, FormGroup, FormLabel, FormControl, Button } from "react-bootstrap";
+import React, { Component } from "react";
+import {
+  Form,
+  FormGroup,
+  FormLabel,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-const AddAudible = () => {
+export class AddAudible extends Component {
+  state = {
+    title: "",
+    by: "",
+    language: "",
+    audio_file: "",
+  };
 
-  return (
-    <Form>
-      <FormGroup>
-        <FormLabel> Title </FormLabel>
-        <FormControl type="text" placeholder="Enter Title" ></FormControl>
-        <FormLabel> By: </FormLabel>
-        <FormControl type="text" placeholder="Created By" ></FormControl>
-        <FormLabel> Language: </FormLabel>
-        <FormControl type="text" placeholder="Language read in" ></FormControl>
-        <FormLabel> Audible: </FormLabel>
-        <FormControl type="text" placeholder="Audio file here" ></FormControl>
-      </FormGroup>
-      <Button type="submit"> Submit </Button>
-      <Link to="/" className="btn btn-danger ml-2">Cancel</Link>
-    </Form>
-  );
-};
+
+  handleOnChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+    [name]: value,
+    });
+  };
+
+  // handleOnSubmit = (e) => {
+  //   e.preventDefault();
+  //   this.props.addPart(this.state);
+  //   this.setState({
+  //     name: "",
+  //     damage: "",
+  //     ability: "",
+  //     weight: "",
+  //   });
+  // };
+
+
+  render() {
+    return (
+      <div>
+        <Form >
+          <FormGroup>
+            <FormLabel> Title </FormLabel>
+            <FormControl type="text" placeholder="Enter Title" value={this.state.name} onChange={this.handleOnChange} name="name"></FormControl>
+            <FormLabel> By: </FormLabel>
+            <FormControl type="text" placeholder="Created By" value={this.state.name} onChange={this.handleOnChange} name="name"></FormControl>
+            <FormLabel> Language: </FormLabel>
+            <FormControl
+              type="text"
+              placeholder="Language read in"
+              value={this.state.name} onChange={this.handleOnChange} name="name"
+            ></FormControl>
+            <FormLabel> Audible: </FormLabel>
+            <FormControl
+              type="text"
+              placeholder="Audio file here"
+              value={this.state.name} onChange={this.handleOnChange} name="name"
+            ></FormControl>
+          </FormGroup>
+          <Button type="submit"> Submit </Button>
+          <Link to="/" className="btn btn-danger ml-2">
+            Cancel
+          </Link>
+        </Form>
+      </div>
+    );
+  }
+}
 
 export default AddAudible;
