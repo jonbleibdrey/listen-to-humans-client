ae0160946b550b1d0942df462bce499dee617066
Jonathan Bleibdrey <jonbleibdrey@aol.com>
2021-02-02 16:43:58 -0500

    added css and audible id

--- src/components/ListOfAudibles.js
+++ src/components/ListOfAudibles.js
@@ -7,15 +7,18 @@ export const ListOfAudibles = ({ title, by, language, audio, id }) => {
   
       <Card style={{ width: "25rem", margin: "40px" }}>
         <Card.Body className="card text-center">
-          <Card.Title>{title}</Card.Title>
+          <Card.Title> title of book: {title}</Card.Title>
+          <hr/>
           <Card.Subtitle className="mb-2 text-muted">
             Created by: {by}
           </Card.Subtitle>
+          <hr/>
           <Card.Text>
             Language: {language}
             <br />
             Audio: {audio}
           </Card.Text>
+          <hr/>
           <Link
             className="btn btn-warning mr-1 btn-lg btn-block"
             to={`/edit/${id}`}
--- src/components/ListOfReviews.js
+++ src/components/ListOfReviews.js
@@ -6,14 +6,18 @@ const ListOfReview = ({id, title, description, rating, audibleId}) => {
     return (
         <Card style={{ width: "25rem", margin: "40px" }}>
         <Card.Body className="card text-center">
-          <Card.Title>{title}</Card.Title>
+          <Card.Title> title of review: {title}</Card.Title>
+          <hr/>
           <Card.Subtitle className="mb-2 text-muted">
             description: {description}
           </Card.Subtitle>
+          <hr/>
           <Card.Text>
             rating: {rating}
             <br />
+            audible id : {audibleId}
           </Card.Text>
+          <hr/>
           <Link
             className="btn btn-warning mr-1 btn-lg btn-block"
             to={`/edit/${id}`}
