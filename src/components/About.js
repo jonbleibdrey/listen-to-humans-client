a13888ae304f1cda175bc087fc0228fa584f321e
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-02-12 15:03:45 -0500

    added dummy datat to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 
 const About = () => {
+    const [people, setPeople] = useState([{
+      name: "tiffany",
+      occupation:"student",
+      bio:" i really like this website it helps me share my college books with others",
+    },{
+      name: "franky",
+      occupation:"lawyer",
+      bio:" I use this sight to help my peers learn all the law books in our library.",
+    },{
+      name: "lang",
+      occupation:"doctor",
+      bio:" i come back and forth to this website to listen to all the medical books others have recorded before me.", 
+    }])
+
+    
   return (
-    <Jumbotron>
+    <Jumbotron style={{ display: "flex", margin: "40px", marginRight: "100px", width: "100%", height:"100%", backgroundColor:"white"}}>
       <Container>
         <h1> About page</h1>
         <p>
@@ -15,6 +30,9 @@ const About = () => {
           people. where i see this shining is college students. It can be
           challenging sometimes to find these books in audible form.{" "}
         </p>
+        <div>
+        
+        </div>
       </Container>
     </Jumbotron>
   );
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,42 +1,49 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import ListOfReviews from "./ListOfReviews";
 import { Container, Row } from "react-bootstrap";
 
 const Review = () => {
+  const [review, setReview] = useState([]);
 
-    const [review, setReview] = useState([])
-    
-    useEffect(() => {
-        axios
-          .get("http://localhost:3001/reviews")
-          .then((resp) => setReview(resp.data));
-      }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/reviews")
+      .then((resp) => setReview(resp.data));
+  }, []);
 
   return (
-   <>
-        <h1 className="card text-center ">review page</h1>
-        <Container>
+    <>
+      <h1 className="card text-center ">review page</h1>
+      <Container>
         <Row>
           {review.map((review) => (
             <ListOfReviews
-            key={review.id}
-            id={review.id}
-            title={review.title}
-            description={review.description}
-            rating={review.rating}
-            audibleId={review.audible_id}
+              key={review.id}
+              id={review.id}
+              title={review.title}
+              description={review.description}
+              rating={review.rating}
+              audibleId={review.audible_id}
             />
-            
-            ))}
-            <Link to="/addReview" style={{ display: "flex", margin: "50px", marginLeft: "100px", width: "100%"}} className="btn btn-primary ml-25 btn-lg ">
+          ))}
+          <Link
+            to="/addReview"
+            style={{
+              display: "flex",
+              margin: "50px",
+              marginLeft: "100px",
+              width: "100%",
+            }}
+            className="btn btn-primary ml-25 btn-lg "
+          >
             {" "}
             add review{" "}
           </Link>
-          </Row>
-          </Container>
-     </>
+        </Row>
+      </Container>
+    </>
   );
 };
 export default Review;
