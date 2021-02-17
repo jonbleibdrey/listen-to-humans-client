e51481339fd6ffe2e22507faf3e659945e1ea814
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-02-17 14:21:22 -0500

    added styling to navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,20 @@
-import React from 'react'
+import React from "react";
 import * as ReactBootStrap from "react-bootstrap";
 
 const Footer = () => {
-    return (
-        
-        <ReactBootStrap.Navbar
-        expand="lg"
-        bg="dark"
-        variant="dark"
-        style={{position:"absolute", left: 0, right: 0, bottom: 0}}
-      >
-        <ReactBootStrap.Navbar.Brand href="/">
-          Read By Humans
-        </ReactBootStrap.Navbar.Brand>
-        <h1> made by: Jonathan  Bleibdrey </h1>
-      </ReactBootStrap.Navbar>
-       
-    )
-}
+  return (
+    <ReactBootStrap.Navbar
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      style={{ position: "absolute", left: 0, right: 0}}
+    >
+      <ReactBootStrap.Navbar.Brand href="/">
+        Read By Humans
+      </ReactBootStrap.Navbar.Brand>
+      <h1> made by: Jonathan Bleibdrey </h1>
+    </ReactBootStrap.Navbar>
+  );
+};
 
-export default Footer
+export default Footer;
