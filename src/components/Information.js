0fb4d47f63ef55638a87c746a0acbdef6371b5fe
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-02-17 15:08:49 -0500

    adding to homepage

--- src/components/Information.js
+++ src/components/Information.js
@@ -2,12 +2,17 @@ import React from "react";
 
 const Information = () => {
   return (
-    <div style={{margin:"3%", border:"2px yellow"}}>
-      <h6>
-        This is a website to listen to audio books from all walks of life. if
-        your a collage student trying to find a book, if your a doctor trying to
+    <div style={{margin:"4%", padding:"40px" , marginLeft: "22%", width:"60%", border: "1px solid gray",fontFamily:"monospace"}}>
+        <img src="../logo192.png" alt="logo" style={{width:"50px", height:"50px", float:"left" }}/>
+      <h4>
+        This is a website to listen to audio books from all walks of life.
+        </h4>
+        <br/>
+        <p>
+         if you're a college student trying to find a book, if your a doctor trying to
         find a book on how to do surgery WE GOT IT and if your a tech guy looking for how to do something we have it!
-      </h6>
+        </p>
+
     </div>
   );
 };
