05ace87065e610b6e71b209779ff0ab0de589211
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-02-18 10:54:26 -0500

    added styling to add audible

--- src/components/AddAudible.js
+++ src/components/AddAudible.js
@@ -43,59 +43,91 @@ export class AddAudible extends Component {
 
   render() {
     return (
-      <Container style={{  width:"100%",
-      height: "auto",
-      bottom: "20px",
-      top: "200px",
-      left: "50px",
-      right: "50px",
-      position: "absolute"}}>
-        <h1 >Add Audible</h1>
-        <Row >
-          <Col xs={12} >
-        <Form onSubmit={this.handleOnSubmit} >
-          <FormGroup >
-            <FormLabel> Title </FormLabel>
-            <FormControl
-              type="text"
-              placeholder="Enter Title"
-              value={this.state.title}
-              onChange={this.handleOnChange}
-              name="title"
-            ></FormControl>
-            <FormLabel> By: </FormLabel>
-            <FormControl
-              type="text"
-              placeholder="Created By"
-              value={this.state.by}
-              onChange={this.handleOnChange}
-              name="by"
-            ></FormControl>
-            <FormLabel> Language: </FormLabel>
-            <FormControl
-              type="text"
-              placeholder="Language read in"
-              value={this.state.language}
-              onChange={this.handleOnChange}
-              name="language"
-            ></FormControl>
-            <FormLabel> Audible: </FormLabel>
-            <FormControl
-              type="text"
-              placeholder="Audio file here"
-              value={this.state.audio_file}
-              onChange={this.handleOnChange}
-              name="audio_file"
-            ></FormControl>
-          </FormGroup>
-          <Button type="submit"> Submit </Button>
-          <Link to="/" className="btn btn-danger ml-2">
-            Cancel
-          </Link>
-        </Form>
-        </Col>
-        </Row>
+      <>
+        <div
+          style={{
+            margin: "40px",
+            padding: "3%",
+            marginLeft: "20%",
+            width: "60%",
+            height: "100%",
+            backgroundColor: "white",
+            border: "1px solid gray",
+            fontFamily: "monospace",
+          }}
+        >
+          <h1>Add Audible</h1>
+          <p>
+            {" "}
+            Here we clearly, add new audibles. its simple you can either import
+            one that you have recorded else where. BUT, you can also record
+            straight from here.
+          </p>
+        </div>
+        <Container style={{ margin: "3%", marginLeft: "380px" }}>
+          <Row>
+            <Col xs={12}>
+              <Form onSubmit={this.handleOnSubmit}>
+                <FormGroup>
+                  <FormLabel> Title </FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Enter Title"
+                    value={this.state.title}
+                    onChange={this.handleOnChange}
+                    name="title"
+                  ></FormControl>
+                  <FormLabel> By: </FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Created By"
+                    value={this.state.by}
+                    onChange={this.handleOnChange}
+                    name="by"
+                  ></FormControl>
+                  <FormLabel> Language: </FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Language read in"
+                    value={this.state.language}
+                    onChange={this.handleOnChange}
+                    name="language"
+                  ></FormControl>
+                  <FormLabel> Audible: </FormLabel>
+                  <FormControl
+                    type="text"
+                    placeholder="Audio file here"
+                    value={this.state.audio_file}
+                    onChange={this.handleOnChange}
+                    name="audio_file"
+                  ></FormControl>
+                </FormGroup>
+                <Button type="submit"> Submit </Button>
+                <Link to="/" className="btn btn-danger ml-2">
+                  Cancel
+                </Link>
+              </Form>
+            </Col>
+          </Row>
         </Container>
+        <div style={{
+            margin: "40px",
+            padding: "3%",
+            marginLeft: "20%",
+            marginBottom:"9%" ,
+            width: "60%",
+            height: "100%",
+            backgroundColor: "white",
+            border: "1px solid gray",
+            fontFamily: "monospace",
+          }}>
+        <h1>Record audio</h1>
+          <p>
+            {" "}
+            Here we can record our book, then simply add to the new audible. This feature is coming soon!
+          </p>
+        </div>
+      </>
     );
   }
 }
