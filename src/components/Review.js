a3bc10ebf1f4d4b2a7630cd8c1f27f2f95594a4a
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-02-18 10:44:07 -0500

    read section in review

--- src/components/Review.js
+++ src/components/Review.js
@@ -20,11 +20,23 @@ const Review = () => {
 
   return (
     <>
-      <div>
+      <div
+        style={{
+          margin: "40px",
+          padding: "4%",
+          marginLeft: "20%",
+          width: "60%",
+          height: "100%",
+          backgroundColor: "white",
+          border: "1px solid gray",
+          fontFamily: "monospace",
+        }}
+      >
+        <h2>Review section</h2>
         <p>
-          This is for the Review section, We want the books to be great and
-          readable. so we set up a system to do just that. Plain and simple nyou
-          can review any book you want. You can also search for any review you would like.
+          We want the books to be great and readable. so we set up a system to
+          do just that. Plain and simple nyou can review any book you want. You
+          can also search for any review you would like.
         </p>
       </div>
       <input
