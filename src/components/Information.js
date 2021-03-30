1d755194a67ef9c45c49b157c47751736853e779
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-03-30 11:23:57 -0400

    changing text for information component

--- src/components/Information.js
+++ src/components/Information.js
@@ -27,12 +27,11 @@ const Information = () => {
         />
       </div>
       <h1 className="animate__animated animate__bounceInDown">
-        A website to listen and create audio books, even if you don't sound like
-        morgan freeman.
+        A website to search google api for books.
       </h1>
       <hr />
       <h6>
-        We got audible books! If you're a college student trying to find a book
+        We got books! If you're a college student trying to find a book
         for your next exam, If your a doctor trying to find a book on how to do
         surgery, even If your a tech guy looking for how to do data and
         algorithms, WE GOT IT!
