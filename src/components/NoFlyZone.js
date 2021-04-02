cd27e54889281fc0f8424b8449a2faf07070cc91
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-04-02 15:48:31 -0400

    added styling to error section

--- src/components/NoFlyZone.js
+++ src/components/NoFlyZone.js
@@ -1,20 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../App.css";
+
 
 const NoFlyZone = () => {
   return (
     <div
       style={{
-        backgroundColor: "#ebd078",
+        backgroundImage: "url(../hallway.jpg)",
+        backgroundSize: "contain",
         padding: "20%",
         color: "red",
         fontSize: "100%",
       }}
     >
-      <h1>NO FLY ZONE!</h1>
+      <img
+        className="animate__animated animate__fadeIn"
+        src="../logo1.PNG"
+        alt="logo"
+        style={{
+          width: "30%",
+          height: "5%",
+          marginLeft: "37%",
+        }}
+      />
+      <h1 style={{
+          height: "5%",
+          float: "center",
+          marginLeft: "10%",
+          fontSize:"100px",
+          color:"black",
+          fontFamily: "monospace",
+            boxShadow: "10px 20px",
+            borderRadius: "20px",
+            backgroundColor:"white",
+            textAlign:"center"
+        }}>NO FLY ZONE!</h1>
       <hr />
-      <Link to="/">...would you like to go back to the home page?</Link>
+      <Link style={{
+          width: "30%",
+          height: "5%",
+          float: "center",
+          marginLeft: "37%",
+          color:"black"
+        }}to="/">...would you like to go back to the home page?</Link>
     </div>
   );
 };
